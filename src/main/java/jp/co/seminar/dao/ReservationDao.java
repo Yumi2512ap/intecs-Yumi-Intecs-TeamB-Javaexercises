e1b41b6761c2aa4e1582396dccb7bc5839ed6b74
intecs <intecs.2512.O4@gmail.com>
2026-04-13 15:32:23 +0900

    Dao追加

--- src/main/java/jp/co/seminar/dao/ReservationDao.java
+++ src/main/java/jp/co/seminar/dao/ReservationDao.java
@@ -10,15 +10,14 @@ import java.util.List;
 import jp.co.seminar.beans.ReservationBean;
 import jp.co.seminar.util.MRConnectionProvider;
 
-public class ReservationDAO {
+public class ReservationDao {
 	//コンストラクタ
-	private ReservationDAO() {
+	public ReservationDao() {
 	}
 
 	//利用日による予約情報取得 
 
 	public List<ReservationBean> findByDate(String date) {
-		//////利用日を指定し、該当日の予約情報を取得する
 		//DB取得結果を格納 
 		List<ReservationBean> List = new ArrayList<ReservationBean>();
 		//データベース接続
@@ -28,44 +27,41 @@ public class ReservationDAO {
 		try (
 				Connection conn = MRConnectionProvider.getConnection();
 				PreparedStatement pstmt = conn.prepareStatement(sql)) {
+			//プレスホルダー(?)に値を設定
 			pstmt.setString(1, date);
 			//SQL文を実行して結果を取得
 			try (ResultSet rs = pstmt.executeQuery()) {
-
-				List<ReservationBean> list = new ArrayList<>();
-
 				//結果セットをviewへ送るための準備
 				while (rs.next()) {
 					//結果セットから取得
-					int intid=rs.getInt("id");
-					String StringroomId=rs.getString("roomId");
-					String strdate=rs.getString("date");
-					String strstart=rs.getString("start");
-					String strend=rs.getString("end");
-					String struserId=rs.getString("userId");
+					int intid = rs.getInt("id");
+					String StringroomId = rs.getString("roomId");
+					String strdate = rs.getString("date");
+					String strstart = rs.getString("start");
+					String strend = rs.getString("end");
+					String struserId = rs.getString("userId");
 					//ReservationBeanオブジェクトを生成し、リストに追加
-					ReservationBean rese=new ReservationBean(intid,StringroomId,strdate,strstart,strend,struserId);
-					
-				
-					list.add(rese);
-					//見つからない場合は、空リスト
-					//エラーの場合は、nullを出力する
-					
+					ReservationBean rese = new ReservationBean(intid, StringroomId, strdate, strstart, strend,
+							struserId);
+
+					List.add(rese);
 				}
-			} catch (Exception e) {
-
 			}
-			//プレスホルダーに値を設定
-			pstmt.setString(1, date);
-			//beanのString型を取得 
-			String dateStr = ReservationBean.getDate();
-
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			System.err.println("ドライバが見つかりません。");
+		} catch (SQLException e) {
+			e.printStackTrace();
+			System.err.println("SQLに関するエラーです");
 		}
-
+	//try-with-resourcesによりconn,pstmtは自動的にクローズされる
+	return List;//画面に返す
 	}
+	
 
-	//--予約情報を格納する　String→date
-	public boolean insert(reservation Reservation) {
+	//--予約情報を格納する　
+	public boolean insert(ReservationBean reservation) {
+
 		//-1が帰ってきたら格納できていない P31参考
 		int ret = -1;
 
@@ -73,26 +69,45 @@ public class ReservationDAO {
 		try (Connection conn = MRConnectionProvider.getConnection();
 				PreparedStatement pstmt = conn.prepareStatement(sql)) {
 			//SQL文を実行
-			ret = pstmt.executeUpdate();
-			System.out.println(ret + "件、挿入しました");
+			int autoIncrementKey = 0;
+			ret = pstmt.executeUpdate(sql, PreparedStatement.RETURN_GENERATED_KEYS);
+			ResultSet rs = pstmt.getGeneratedKeys();
+			return ret != 0;
+
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			System.err.println("ドライバが見つかりません。");
+			return false;
 		} catch (SQLException e) {
 			e.printStackTrace();
 			System.err.println("SQLに関するエラーです");
+			return false;
 		}
-	} //try-with-resourcesによりconnとpstmtは自動的にクローズされる
+	}
+
+	//try-with-resourcesによりconnとpstmtは自動的にクローズされる
 
 	//--予約情報を削除する  P37
-	public boolean delete(reservation Reservation) {
-
+	public boolean delete(ReservationBean reservation) {
+		int ret = -1;
+		//SQLでデータベース削除
 		String sql = "DELETE FROM meetingroom WHERE date = ?";
+		//try-with-resources構文でリソースを自動的にクローズ
 		try (Connection conn = MRConnectionProvider.getConnection();
 				PreparedStatement pstmt = conn.prepareStatement(sql)) {
 			//SQL文を実行
 			ret = pstmt.executeUpdate();
-			System.out.println(ret + "件、削除しました");
+			return ret != 0;
+
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			System.err.println("ドライバが見つかりません。");
+			return false;
 		} catch (SQLException e) {
 			e.printStackTrace();
 			System.err.println("SQLに関するエラーです");
-		}
+			return false;
+		} //try-with-resourcesによりconnとpstmtは自動的にクローズされる
+
 	}
 }
