b0e0896be48b1c02103e96a0710601d9699f9d3a
user <intecs.2512ap.09@gmail.com>
2026-04-13 15:39:27 +0900

    RoomDao８割

---
+++ src/main/java/jp/co/seminar/dao/RoomDao.java
@@ -0,0 +1,47 @@
+package jp.co.seminar.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import jp.co.seminar.beans.RoomBean;
+import jp.co.seminar.util.MRConnectionProvider;
+
+public class RoomDao {
+	// インスタンス化を防ぐため、コンストラクタをprivateで定義
+	private RoomDao() {
+	}
+
+	public static RoomBean[] findAll() {
+		String id;
+		String name;
+		// SELECTの結果格納用の配列を用意
+		// 拡張可能なリストの作成
+		List<RoomBean> nameDataList = new ArrayList<RoomBean>();
+		// パラメータを含む動的SQL文を用意
+		String sql = "SELECT * FROM room where id";
+		// try-with-resourcesでリソースを自動的にクローズ
+		try (Connection conn = MRConnectionProvider.getConnection();
+				PreparedStatement pstmt = conn.prepareStatement(sql)) {
+			// SQL文を実行して結果を取得
+			ResultSet rs = pstmt.executeQuery();
+			// 結果を表示
+			while (rs.next()) {
+				id = rs.getString("id");
+				name = rs.getString("name");
+				nameDataList.add(new RoomBean(id, name));
+				// サイズ→RoomBean[]に
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			System.out.println("SQLに関するエラーです。");
+			return null;
+
+		}
+		return rom();//  会議室の配列（見つからない場合は、nullを返却）
+
+	}
+}
