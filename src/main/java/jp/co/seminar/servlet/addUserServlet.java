1437749febb3eccd15830a733497762518be2d2d
umemura <intecs.2512ap.10@gmail.com>
2026-04-14 09:56:19 +0900

    サーブレット記述

--- src/main/java/jp/co/seminar/servlet/addUserServlet.java
+++ src/main/java/jp/co/seminar/servlet/addUserServlet.java
@@ -7,6 +7,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import jp.co.seminar.beans.MeetingRoom;
 
 /**
  * Servlet implementation class addUserServlet
@@ -14,19 +17,20 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/addUser")
 public class addUserServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public addUserServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public addUserServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.sendRedirect("login.jsp");
 		return;
@@ -35,9 +39,20 @@ public class addUserServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		
+		request.setCharacterEncoding("UTF-8");
+		// セッションからミーティングルームを取得
+		HttpSession session = request.getSession();
+		MeetingRoom MR = (MeetingRoom) session.getAttribute("meetingRoom");
+
+		// JSPからデータを受け取る
+		String userId = request.getParameter("userId");
+		String userName = request.getParameter("userName");
+		String userPw = request.getParameter("userPw");
+		String address = request.getParameter("address");
+
 	}
 
 }
