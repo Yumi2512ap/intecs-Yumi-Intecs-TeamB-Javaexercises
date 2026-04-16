3e664bbf183c60e0dcd8a5c79c9386782e844f92
umemura <intecs.2512ap.10@gmail.com>
2026-04-16 09:26:19 +0900

    コンテキストパスを変更

---
+++ src/main/java/jp/co/seminar/util/LoginFilter.java
@@ -0,0 +1,61 @@
+package jp.co.seminar.util;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Servlet Filter implementation class LoginFilter
+ */
+@WebFilter("/*")
+public class LoginFilter extends HttpFilter implements Filter {
+       
+    /**
+     * @see HttpFilter#HttpFilter()
+     */
+    public LoginFilter() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		// place your code here
+		try {
+			HttpServletRequest httpRequest = (HttpServletRequest) request;
+			String rURI = httpRequest.getRequestURI();
+			
+			chain.doFilter(request, response);
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		// pass the request along the filter chain
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
