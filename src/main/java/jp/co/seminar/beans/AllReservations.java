56ebb9113b5991b2f60fb0199ba910eb17ea0772
umemura <intecs.2512ap.10@gmail.com>
2026-04-17 13:55:54 +0900

    予約全件のソート機能追加（未完成

---
+++ src/main/java/jp/co/seminar/beans/AllReservations.java
@@ -0,0 +1,14 @@
+package jp.co.seminar.beans;
+
+import java.io.Serializable;
+
+public class AllReservations implements Serializable {
+	private String order;
+	private String date1;
+	private String date2;
+	private String room;
+	private String user;
+	
+	public AllReservations() {
+	}
+}
