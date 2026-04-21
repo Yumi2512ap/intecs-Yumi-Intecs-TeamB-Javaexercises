80e8fed3ac6b61970f542be1ee1e9b1aa37cd4a5
katou <intecs.2512ap.12@gmail.com>
2026-04-21 13:52:57 +0900

    DAOの修正　サーブレットは後ほど

--- src/main/java/jp/co/seminar/dao/ImageDao.java
+++ src/main/java/jp/co/seminar/dao/ImageDao.java
@@ -3,11 +3,38 @@ package jp.co.seminar.dao;
 import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.Timestamp;
 
+import jp.co.seminar.beans.ImageBean;
 import jp.co.seminar.util.MRConnectionProvider;
 
 public class ImageDao {
+
+	public ImageBean findById(int roomId) throws Exception {
+		String sql = "SELECT image_id, room_id, image_name, image_type, image_content, image_size, created_at "
+				+ "FROM image WHERE image_id = ?";
+
+		try (Connection conn = MRConnectionProvider.getConnection();
+				PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+			pstmt.setInt(1, roomId);
+
+			try (ResultSet rs = pstmt.executeQuery()) {
+				if (rs.next()) {
+					return new ImageBean(
+							rs.getInt("image_id"),
+							rs.getString("room_id"),
+							rs.getString("image_name"),
+							rs.getString("image_type"),
+							rs.getBytes("image_content"),
+							rs.getInt("image_size"),
+							rs.getTimestamp("created_at"));
+				}
+			}
+		}
+		return null;
+	}
 
 	public void insertImage(String roomId, String imageName, String imageType,
 			InputStream imageContent, int imageSize, Timestamp createdAt) throws Exception {
